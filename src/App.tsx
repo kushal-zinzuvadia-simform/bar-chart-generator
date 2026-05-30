9218faf3ae24372c2fa60b5124fd611f2f8a0776
Kushal Zinzuvadia <kushal.zinzuvadia@simformsolutions.com>
2026-05-30 15:09:51 +0530

    feat: Handle form data input, add data functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import InputForm from './components/InputForm/InputForm';
 
 export interface ChartItem {
@@ -9,10 +9,14 @@ export interface ChartItem {
 function App() {
   const [chartData, setChartData] = useState<ChartItem[]>([]);
 
+  useEffect(() => {
+    console.log(chartData);
+  }, [chartData]);
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center gap-6 px-4">
       <h2 className="text-2xl font-semibold">Create a Bar Chart</h2>
-      <InputForm />
+      <InputForm setChartData={setChartData} />
     </div>
   );
 }
--- src/components/InputForm/InputForm.tsx
+++ src/components/InputForm/InputForm.tsx
@@ -1,13 +1,43 @@
-const InputForm = () => {
+import { useRef } from 'react';
+import type { ChartItem } from '../../App';
+
+type InputFormProps = {
+  setChartData: React.Dispatch<React.SetStateAction<ChartItem[]>>;
+};
+
+const InputForm = ({ setChartData }: InputFormProps) => {
+  const labelRef = useRef<HTMLInputElement>(null);
+  const valueRef = useRef<HTMLInputElement>(null);
+
+  const handleInput = (e: React.SubmitEvent) => {
+    e.preventDefault();
+
+    const label = labelRef.current?.value.trim() ?? '';
+    const value = valueRef.current?.value ?? '';
+    if (!label || value === '') return;
+
+    setChartData((prev: ChartItem[]) => [
+      ...prev,
+      {
+        label,
+        value: Number(value),
+      },
+    ]);
+
+    if (labelRef.current) labelRef.current.value = '';
+    if (valueRef.current) valueRef.current.value = '';
+  };
+
   return (
     <div className="p-4">
-      <form className="flex items-center gap-3">
+      <form className="flex items-center gap-3" onSubmit={handleInput}>
         <label htmlFor="inputX" className="text-sm font-medium">
           X:
         </label>
         <input
           id="inputX"
           type="text"
+          ref={labelRef}
           className="border rounded px-3 py-2 text-sm outline-none focus:ring-1"
         />
 
@@ -17,6 +47,7 @@ const InputForm = () => {
         <input
           id="inputY"
           type="number"
+          ref={valueRef}
           className="border rounded px-3 py-2 text-sm outline-none focus:ring-1"
         />
 
